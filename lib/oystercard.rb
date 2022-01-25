00bd7a6901427976608a38b5bdf86945b9fea9f0
john connolly <johnc1983@hotmail.co.uk>
2022-01-25 17:24:07 +0000

    Dream team have arrived

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,25 +3,24 @@ class Oystercard
     MAXIMUM_BALANCE = 90
     MINIMUM_BALANCE = 1
 
-    attr_reader :balance
+    attr_reader :balance, :entry_station
   
     def initialize(balance = 0)
         @balance = balance
-        @in_journey = false
     end
 
     def in_journey?
-        @in_journey
+        @entry_station
     end
 
-    def touch_in
+    def touch_in(entry_station) 
         fail "No Balance" if @balance < MINIMUM_BALANCE
-        @in_journey = true
+        @entry_station = entry_station
     end
 
     def touch_out
         deduct(MINIMUM_BALANCE)
-        @in_journey = false
+        @entry_station = nil
     end
 
     def top_up(money)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,7 @@ describe Oystercard do
     
     let(:oystercard) { Oystercard.new }
     let(:oystercard50) { Oystercard.new(50)} 
-
+    let(:entry_station) { double :station}
     it '#\'s initializes to 0' do
         expect(oystercard.balance).to eq 0
     end
@@ -23,29 +23,42 @@ describe Oystercard do
     end
 
     it '#is in journey?' do
-        expect(oystercard.in_journey?).to be false
+        expect(oystercard.in_journey?).to be nil 
     end
 
     it '#allows us to touch in' do
-        oystercard50.touch_in
+        oystercard50.touch_in(entry_station)
 
-        expect(oystercard50.in_journey?).to be true 
+        expect(oystercard50.in_journey?).to be entry_station
     end
 
     it '#allows us to touch out' do
-        oystercard50.touch_in
+        oystercard50.touch_in(entry_station)
 
         oystercard50.touch_out
 
-        expect(oystercard50.in_journey?).to be false
+        expect(oystercard50.in_journey?).to be nil
     end
 
     it '#will not allows touch in with 0 balance' do
-        expect{oystercard.touch_in}.to raise_error "No Balance"
+        expect{oystercard.touch_in(entry_station)}.to raise_error "No Balance"
     end
 
     it '#charges on touch out' do
         expect{oystercard50.touch_out}.to change{oystercard50.balance}.by(-1)
     end 
+
+    it '#expects the card to remember the entry station' do
+        expect(oystercard50.touch_in(entry_station)).to eq entry_station
+    end
+
+    it '#expects card to forget the entry station on touch out' do
+        oystercard50.touch_in(entry_station)
+        oystercard50.touch_out
+        expect(oystercard50.entry_station).to eq nil
+    end
+
+
+
 
 end
